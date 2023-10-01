716c600010c4e46d9ab8887fe9db12372f270425
RexBerry <rex.berry@mst.edu>
2023-10-01 02:09:39 -0500

    Rebalance summary

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@
 internal class Program
 {
     public const int SIZE_THRESHOLD = 250;
-    public const int SUBMISSION_PAIRS_TO_DISPLAY = 15;
-    public const int SUBMISSIONS_TO_DISPLAY = 10;
+    public const int SUBMISSION_PAIRS_TO_DISPLAY = 20;
+    public const int SUBMISSIONS_TO_DISPLAY = 5;
 
     private static void Main(string[] args)
     {
